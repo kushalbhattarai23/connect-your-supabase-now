6a43079767eb6ec806e690cb121238159413e53c
kushalbhattarai23 <45483709+kushalbhattarai23@users.noreply.github.com>
2025-06-12 12:04:50 +0545

    Update currencies.ts

--- src/config/currencies.ts
+++ src/config/currencies.ts
@@ -2,11 +2,11 @@
 import { Currency } from '@/types';
 
 export const currencies: Currency[] = [
-  { code: 'NPR', symbol: 'रु', name: 'Nepalese Rupee' },
+  { code: 'NPR', symbol: 'रु', name: 'Nepalese Rupees' },
   { code: 'USD', symbol: '$', name: 'US Dollar' },
   { code: 'EUR', symbol: '€', name: 'Euro' },
   { code: 'GBP', symbol: '£', name: 'British Pound' },
-  { code: 'INR', symbol: '₹', name: 'Indian Rupee' },
+  { code: 'INR', symbol: '₹', name: 'Indian Rupees' },
   { code: 'JPY', symbol: '¥', name: 'Japanese Yen' },
 ];
 
